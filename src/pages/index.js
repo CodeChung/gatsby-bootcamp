6a2c86cc45a5fdd4490fc829511a1acd137ab106
Harry Chung <hc9825@gmail.com>
2019-10-22 21:48:54 -0700

    home page link removal

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
             <Head title='Home' />
             <h1>Hello.</h1>
             <h2>I'm Harry, a full-stack developer living in LA</h2>
-            <p>Need a developer? <Link to='/contact'>Contact me.</Link></p>
         </Layout>
     )
 }
