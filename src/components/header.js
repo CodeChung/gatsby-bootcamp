57bd7d8e5e235c7543dab682c2e0553070835d02
Harry Chung <hc9825@gmail.com>
2019-10-23 22:09:42 -0700

    beautiful lil nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import { FiCoffee, FiHome,  } from 'react-icons/fi'
+import { FiCoffee, FiHome, FiFeather, FiUmbrella,   } from 'react-icons/fi'
+import { TiThumbsOk } from 'react-icons/ti'
 
 import headerStyles from './header.module.scss'
 import HoverIcon from './HoverIcon/hoverIcon'
 
+// this is where I'm storing the icons for each page
+const pageIcons = {
+    home: <FiHome />,
+    projects: <TiThumbsOk />,
+    about: <FiUmbrella />,
+    blog: <FiFeather />,
+    contact: <FiCoffee />,
+}
+
 const Header = (props) => {
+
     const data = useStaticQuery(graphql`
         query {
             site {
@@ -17,6 +28,7 @@ const Header = (props) => {
         }
     `)
 
+    const { active, view } = props
     return (
         <header className={headerStyles.header}>
             <h1>
@@ -25,12 +37,22 @@ const Header = (props) => {
                 </AniLink>
             </h1>
             <nav>
-                <div className={headerStyles.hamburger} />
-                <ul className={headerStyles.navList}>
-                    <li><AniLink paintDrip duration={.87} hex="#000000" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to='/'><HoverIcon icon={<FiHome />} title='Home' /></AniLink></li>
-                    <li><AniLink paintDrip duration={.87} hex="#71e355" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to ='/about'><HoverIcon title='About' /></AniLink></li>
-                    <li><AniLink paintDrip duration={.87} hex="#00ffc6" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to ='/contact'><HoverIcon title='Contact' /></AniLink></li>
-                    <li><AniLink paintDrip duration={.87} hex="#9db4f6" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to ='/blog'><HoverIcon title='Blog' /></AniLink></li>
+                <button 
+                    onClick={props.toggleNav}
+                    className={headerStyles.currentIcon}>{pageIcons[view]}</button>
+                <ul
+                    onClick={() => {
+                        if (!active) {
+                            props.toggleNav()
+                        }
+                    }}
+                    className={headerStyles.navList + ' ' + (active && headerStyles.navListActive)}>
+                    <li className={headerStyles.ghostLink + (active && ' active')}><AniLink paintDrip duration={.87} hex="#000000" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to={`/${view}`}><HoverIcon active={active} view={view} icon={pageIcons.home} title='Home' /></AniLink></li>
+                    <li><AniLink paintDrip duration={.87} hex="#000000" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to='/'><HoverIcon active={active} view={view} icon={pageIcons.home} title='Home' /></AniLink></li>
+                    <li><AniLink paintDrip duration={.87} hex="#000000" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to='/'><HoverIcon active={active} view={view} icon={pageIcons.projects} title='Projects' /></AniLink></li>
+                    <li><AniLink paintDrip duration={.87} hex="#71e355" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to ='/about'><HoverIcon active={active} view={view} icon={pageIcons.about} title='About' /></AniLink></li>
+                    <li><AniLink paintDrip duration={.87} hex="#00ffc6" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to ='/contact'><HoverIcon active={active} view={view} icon={pageIcons.contact} title='Contact' /></AniLink></li>
+                    <li><AniLink paintDrip duration={.87} hex="#9db4f6" className={headerStyles.navItem + ' ' + headerStyles[props.view]} activeClassName={headerStyles.activeNavItem} to ='/blog'><HoverIcon active={active} view={view} icon={pageIcons.blog} title='Blog' /></AniLink></li>
                 </ul>
             </nav>
         </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,11 +6,32 @@ import layoutStyles from './layout.module.scss'
 
 
 class Layout extends React.Component {
+    state = {
+        currentView: 'home',
+        activeNav: false,
+        
+    }
+    componentDidMount() {
+        if (this.props.view) {
+            this.setView({ currentView: this.props.view })
+        }
+    }
+    setView = (view) => {
+        this.setState({ currentView: view })
+    }
+    toggleNav = () => {
+        this.setState({ activeNav: !this.state.activeNav })
+    }
     render() {
+        const { activeNav } = this.state
         return(
             <div className={`${layoutStyles.container} ${layoutStyles[this.props.view]}`}>
                 <div className={layoutStyles.content}>
-                    <Header view={this.props.view} />
+                    <Header 
+                        active={activeNav}
+                        view={this.props.view} 
+                        setView={this.setView} 
+                        toggleNav={this.toggleNav} />
                     {this.props.children}
                 </div>
                 <Footer />
