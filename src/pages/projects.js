c4fbad73e6b72f899585d4ced7f0815894cc21f1
Harry Chung <hc9825@gmail.com>
2019-10-24 00:03:53 -0700

    projects page set up also look into react event listener

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Projects = () => {
+    return (
+        <Layout view='projects' title='Projects' >
+            Projects List
+        </Layout>
+    )
+}
+
+export default Projects
